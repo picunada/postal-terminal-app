b8444fadb5b9c99db39f6c625c516274a471c0cf
Daniil Bezuglov <daniil.bezuglov@lockerbot.io>
2022-10-15 11:21:23 +0700

    Initial Commit

---
+++ locker-test/locker_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  locker_testApp.swift
+//  locker-test
+//
+//  Created by Danil Bezuglov on 10/15/22.
+//
+
+import SwiftUI
+
+@main
+struct locker_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
